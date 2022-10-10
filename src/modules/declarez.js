01bc22f2a6ccd06556ea2c54ecf268369506bd8a
john <kiokoj81@gmail.com>
2022-10-10 18:00:07 +0300

    todo app code for review

---
+++ src/modules/declarez.js
@@ -0,0 +1,11 @@
+const listChores = document.getElementById('todoList');
+const checkbox = document.querySelector('input[name=checked]');
+const choreDesc = document.querySelector('.choreDesc');
+const addChore = document.querySelector('input[name=addWorks]');
+const clear = document.getElementById('clearAll');
+const clears = document.getElementById('recycle');
+const addChores = document.getElementById('fa-check');
+
+export {
+  listChores, checkbox, choreDesc, addChore, clear, clears, addChores,
+};
